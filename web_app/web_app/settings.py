503d4ac405fd4512651b7439a2c7c10198ecf356
Martin Laubscher <martin.laubscher@me.com>
2023-07-20 16:33:00 +0100

    implemented caching for current weather

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -111,6 +111,13 @@ DATABASES = {
 #     }
 # }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
