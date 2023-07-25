c4501d0c97b92e0cbfdc4069c76556d9f4d1234e
jw-mk <123461637+jw-mk@users.noreply.github.com>
2023-07-25 10:15:07 +0100

    Endpoint validation (#31)
    Updated update_weather_current.py to include default values for relevant fields.
    Updated tests to validate golden_hour and main-form-submission endpoints.
    Includes changes from 'pythonpath' branch.

--- data/database/data_population_scripts/populate_taxi_zones.py
+++ data/database/data_population_scripts/populate_taxi_zones.py
@@ -1,9 +1,21 @@
+import os
+import sys
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+cityframe_path = os.path.dirname(os.path.dirname(os.path.dirname(current_path)))
+taxi_path = os.path.join(current_path, "../..", "GeoJSON", "manhattan_taxi_zones.geojson")
+building_path = os.path.join(current_path, "../..", "GeoJSON", "Building_points.geojson")
+tree_path = os.path.join(current_path, "../..", "GeoJSON", "tree_points.geojson")
+
+sys.path.append(cityframe_path)
+
 from sqlalchemy import create_engine, URL, MetaData, Table
 from sqlalchemy.dialects.postgresql import insert
 import json
 from credentials import pg_conn
 import geopandas as gpd
-from data.Mapping_Buildings_and_Zones.points_in_zones import map_points_to_zones, rank_zones_by_point_presence, map_to_scale
+from data.Mapping_Buildings_and_Zones.points_in_zones import map_points_to_zones, rank_zones_by_point_presence, \
+    map_to_scale
 
 # SCRIPT TO POPULATE THE TAXI_ZONES TABLE WITH STATIC DATA FROM TAXI ZONES, BUILDINGS, AND TREES GEOJSON FILES
 
@@ -21,9 +33,9 @@ table = Table('taxi_zones', MetaData(), autoload_with=engine, schema='cityframe'
 vals = []
 
 # read files needed to map buildings/trees to taxi zones
-building_points = gpd.read_file("../../GeoJSON/Building_points.geojson")
-tree_points = gpd.read_file("../../GeoJSON/tree_points.geojson")
-zone_polygons = gpd.read_file("../../GeoJSON/manhattan_taxi_zones.geojson")
+building_points = gpd.read_file(building_path)
+tree_points = gpd.read_file(tree_path)
+zone_polygons = gpd.read_file(taxi_path)
 building_feature_filter = 'Style_Prim'
 
 # create dictionaries for buildings/trees per taxi zone
@@ -33,12 +45,11 @@ tree_counts_scaled = map_to_scale(tree_counts_in_zones)
 
 # connect to db
 with engine.begin() as connection:
-
     # clear table
     connection.execute(table.delete())
 
     # load taxi zone geojson
-    with open('../../GeoJSON/manhattan_taxi_zones.geojson', 'r') as f:
+    with open(taxi_path, 'r') as f:
         data = json.load(f)
     # loop through taxi zones and add id + zone name to dictionary for that zone
     for feature in data['features']:
