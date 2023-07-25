d36bd2f7deaf77b031f0496274b57fb43f371c94
Martin Laubscher <martin.laubscher@me.com>
2023-07-25 14:00:52 +0100

    fixed wrong db setting in settings.py

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -106,9 +106,6 @@ DATABASES = {
         'PASSWORD': pg_conn['password'],
         'HOST': pg_conn['host'],
         'PORT': pg_conn['port'],
-        'OPTIONS': {
-            'options': '-c search_path=cityframe'
-        },
     }
 }
 
