c1741824572ca9a9be38cb3aa052f362f3ad86b3
Martin Laubscher <martin.laubscher@me.com>
2023-07-27 19:55:27 +0100

    implemented selenium tests for date/time selection

--- web_app/tests/test_selenium.py
+++ web_app/tests/test_selenium.py
@@ -1,11 +1,93 @@
-import time
+import os
+import sys
 
+current_path = os.path.dirname(os.path.abspath(__file__))
+cityframe_path = os.path.dirname(os.path.dirname(current_path))
+sys.path.append(cityframe_path)
+
+import datetime
+import time
+import re
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.management import call_command
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 import selenium.webdriver.support.expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from dateutil import tz
+from datetime import datetime, timedelta
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from django.db import connections
+from parameterized import parameterized_class
+from data.database.data_population_scripts import apiupdate, update_weather_fc
+from data import machine_learning_app
+
+
+
+# list to store the different browsers
+# browsers = []
+#
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--disable-popup-blocking')
+# chrome = Service(ChromeDriverManager().install())
+# browsers.append((webdriver.Chrome, chrome, chrome_options))
+#
+# firefox_options = webdriver.FirefoxOptions()
+# firefox_options.add_argument('-headless')
+# firefox = Service(GeckoDriverManager().install())
+# browsers.append((webdriver.Firefox, firefox, firefox_options))
+
+class ElementHasValue:
+    def __init__(self, locator):
+        self.locator = locator
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)
+        return element.get_attribute("value") != ""
+
+
+class ElementHasClass(object):
+    def __init__(self, locator, css_class):
+        self.locator = locator
+        self.css_class = css_class
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)
+        # if self.css_class in element.get_attribute("class"):
+        #     return element
+        # else:
+        #     return False
+        return self.css_class == element.get_attribute("class")
+
+
+class TextMatchesPattern(object):
+    def __init__(self, locator, pattern):
+        self.locator = locator
+        self.pattern = pattern
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)
+        if bool(re.match(self.pattern, element.text)):
+            return element
+        else:
+            return False
+
+
+# @parameterized_class([
+#     {
+#         "browser_class": browser_class,
+#         "browser": browser,
+#         "option": option,
+#     }
+#     for browser_class, browser, option in browsers
+# ])
 
 
 class IntegrationTests(StaticLiveServerTestCase):
@@ -37,8 +119,9 @@ class IntegrationTests(StaticLiveServerTestCase):
     test_bottom() -> None:
         Test to check the functionality of the bottom offcanvas in the website.
     """
-
     selenium = None
+    current_time = None
+    upper_bound_time = None
 
     @classmethod
     def setUpClass(cls):
@@ -47,9 +130,31 @@ class IntegrationTests(StaticLiveServerTestCase):
         Initializes Selenium WebDriver and sets an implicit wait.
         """
 
+        update_weather_fc.main()
+        machine_learning_app.main()
+
         super().setUpClass()
-        cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
+
+        chrome_options = webdriver.ChromeOptions()
+        # chrome_options.add_argument('--headless')
+        chrome_driver = Service(ChromeDriverManager().install())
+        chrome = webdriver.Chrome(service=chrome_driver, options=chrome_options)
+
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.add_argument('-headless')
+        firefox_driver = Service(GeckoDriverManager().install())
+        firefox = webdriver.Firefox(service=firefox_driver, options=firefox_options)
+
+        cls.selenium = chrome
+        cls.selenium.implicitly_wait(5)
+        cls.wait = WebDriverWait(cls.selenium, 5)
+        cls.action = ActionChains(cls.selenium)
+        cls.current_time = datetime.utcnow().astimezone(tz=tz.gettz('America/New_York'))
+        cls.lower_exceeding_time = cls.current_time - timedelta(days=1)
+        cls.upper_bound_time = cls.current_time + timedelta(days=15)
+        cls.upper_exceeding_time = cls.upper_bound_time + timedelta(days=1)
+        cls.hour_pattern = r"^(0?[0-9]|1[0-9]|2[0-3])$"
+        cls.dt_selection = None
 
     @classmethod
     def tearDownClass(cls):
@@ -61,51 +166,216 @@ class IntegrationTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_offcanvas_top(self):
+    def _navigate_to_site(self):
         """
-        Tests the functionality of the top dropdown menu in the website.
-        Checks whether the dropdown menu appears and disappears as expected when clicked.
+        Loads the page in Django's test environment.
         """
 
         self.selenium.get(f"{self.live_server_url}")
 
-        dropdown_menu = self.selenium.find_element(By.XPATH, '//*[@id="offcanvasTop"]')
+    def _open_nav_menu(self):
+        """
+        Opens the navigation/top menu. Requires the page to be loaded.
+        """
 
-        # click on dropdown menu button, wait for it to appear, and check if it has updated its class
         self.selenium.find_element(By.CSS_SELECTOR, 'button.btn.btn-dark.menu-button').click()
-        time.sleep(1)
-        self.assertEquals(dropdown_menu.get_attribute("class"), "offcanvas offcanvas-top show")
+        return self.wait.until(
+            ElementHasClass((By.XPATH, '//*[@id="offcanvasTop"]'), 'offcanvas offcanvas-top show'))
 
-        # click on dropdown menu close button, wait for it to disappear, and check if it has updated its class
-        WebDriverWait(self, 10).until(EC.element_to_be_clickable(
+    def _close_nav_menu(self):
+        """
+        Closes the navigation/top menu. Requires the menu to be visible.
+        """
+
+        self.wait.until(EC.element_to_be_clickable(
             self.selenium.find_element(By.CSS_SELECTOR, 'button.btn-close'))).click()
-        time.sleep(1)
-        self.assertEquals(dropdown_menu.get_attribute("class"), "offcanvas offcanvas-top")
+        return self.wait.until(
+            ElementHasClass((By.XPATH, '//*[@id="offcanvasTop"]'), 'offcanvas offcanvas-top'))
 
-    def test_offcanvas_bottom(self):
+    def _open_search_menu(self):
         """
-        Tests the functionality of the bottom offcanvas menu in the website.
-        Checks whether it shows and hides correctly when interacted with.
+        Opens the search/bottom menu. Requires the page to be loaded.
         """
 
-        self.selenium.get(f"{self.live_server_url}")
-
-        # click on bottom offcanvas menu button, wait for it to appear, and check if it has updated its class
         self.selenium.find_element(By.CSS_SELECTOR, '.offcanvas-button').click()
-        time.sleep(1)
-        self.assertEqual(self.selenium.find_element(By.XPATH, '//*[@id="offcanvasBottom"]').get_attribute('class'),
-                         "offcanvas offcanvas-bottom show")
+        return self.wait.until(
+            ElementHasClass((By.XPATH, '//*[@id="offcanvasBottom"]'), 'offcanvas offcanvas-bottom show'))
+
+    def _close_search_menu(self):
+        """
+        Closes the search/bottom menu.
+        """
 
         # calculate point in top 20% of window - needed to make bottom menu disappear
         window_size = self.selenium.get_window_size()
         x_coordinate = window_size["width"] / 2
         y_coordinate = window_size["height"] / 5
 
+        self.action.reset_actions()
+
         # click on previously defined point to close bottom offcanvas menu and wait for it to disappear
-        actions = ActionChains(self.selenium)
-        actions.move_by_offset(x_coordinate, y_coordinate).click().perform()
-        time.sleep(1)
-        # check it disappeared by verifying its class has been updated
-        self.assertEqual(
-            self.selenium.find_element(By.XPATH, '//*[@id="offcanvasBottom"]').get_attribute('class'),
-            "offcanvas offcanvas-bottom")
+        self.action.move_by_offset(x_coordinate, y_coordinate).click().perform()
+        return self.wait.until(
+            ElementHasClass((By.XPATH, '//*[@id="offcanvasBottom"]'), 'offcanvas offcanvas-bottom'))
+
+    def _click_dt_selection(self):
+        """
+        Clicks on the date/time selection element in the search menu. Requires menu to be visible.
+        """
+
+        self.dt_selection = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.form-control')))
+        self.dt_selection.click()
+
+    def _next_month(self):
+        """
+        Clicks on the next month button. Requires date/time selection element to be active/expanded.
+        """
+
+        next_month_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.rdtNext')))
+        self.action.move_to_element(next_month_button).click().perform()
+
+    def _prev_month(self):
+        """
+        Clicks on the previous month button. Requires date/time selection element to be active/expanded.
+        """
+
+        prev_month_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.rdtPrev')))
+        self.action.move_to_element(prev_month_button).click().perform()
+
+    def _switch_to_dt(self):
+        switch_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.rdtSwitch')))
+        self.action.move_to_element(switch_button).click().perform()
+
+    def _click_on_date(self, day, month, year):
+        """
+        Clicks on a specific date within the application's date picker.
+
+        Args:
+            day (int): The day of the month to select. Expected values are 1-31.
+            month (int): The month to select. Expected values are 1-12, where 1
+                represents January and 12 represents December.
+            year (int): The year to select. This should be a valid four-digit year.
+        """
+
+        element = self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                              f'//td[@data-value="{day}" and @data-month="{month - 1}" and @data-year="{year}"]')))
+        element.click()
+
+    def _click_time_selection(self):
+        """
+        Clicks on the time selection button to open the selection. Requires date/time selection element to be active/expanded.
+        """
+
+        time_selection = self.selenium.find_element(By.CSS_SELECTOR, '.rdtTimeToggle')
+        time_selection.click()
+
+    def _next_hour(self):
+        """
+        Clicks on the next hour button. Requires time selection element to be active/expanded.
+        """
+
+        next_hour_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.rdtBtn')))
+        self.action.move_to_element(next_hour_button).click().perform()
+
+    def _prev_hour(self):
+        """
+        Clicks on the previous hour button. Requires time selection element to be active/expanded.
+        """
+
+        prev_hour_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.rdtBtn:nth-child(3)')))
+        self.action.move_to_element(prev_hour_button).click().perform()
+
+    def test_nav_menu_opens(self):
+        self._navigate_to_site()
+        self.assertTrue(self._open_nav_menu())
+
+    def test_nav_menu_closes(self):
+        self._navigate_to_site()
+        self._open_nav_menu()
+        self.assertTrue(self._close_nav_menu())
+
+    def test_search_menu_opens(self):
+        self._navigate_to_site()
+        self.assertTrue(self._open_search_menu())
+
+    def test_search_menu_closes(self):
+        self._navigate_to_site()
+        self._open_search_menu()
+        self.assertTrue(self._close_search_menu())
+
+    def test_lower_bound(self):
+        self._navigate_to_site()
+        self._open_search_menu()
+
+        if self.lower_exceeding_time.month < self.current_time.month or self.lower_exceeding_time.year < self.current_time.year:
+            self._click_dt_selection()
+            self._prev_month()
+
+        assert ElementHasClass((By.XPATH,
+                                f'//td[@data-value="{self.lower_exceeding_time.day}" and @data-month="{self.lower_exceeding_time.month - 1}" and @data-year="{self.lower_exceeding_time.year}"]'),
+                               'rdtDay rdtDisabled')(self.selenium)
+
+    def test_upper_bound(self):
+        self._navigate_to_site()
+        self._open_search_menu()
+
+        # navigate to the month with the last day that can be selected
+        if self.upper_bound_time.month > self.current_time.month or self.upper_bound_time.year > self.current_time.year:
+            self._click_dt_selection()
+            self._next_month()
+            # if the first day that can't be selected is in the month after the last selectable day, go to that month
+            if self.upper_exceeding_time.month > self.upper_bound_time.month or self.upper_exceeding_time.year > self.upper_bound_time.year:
+                self._next_month()
+
+        assert ElementHasClass((By.XPATH,
+                                f'//td[@data-value="{self.upper_exceeding_time.day}" and @data-month="{self.upper_exceeding_time.month - 1}" and @data-year="{self.upper_exceeding_time.year}"]'),
+                               'rdtDay rdtDisabled')(self.selenium)
+
+    def test_hour_rollover_upwards(self):
+        self._navigate_to_site()
+        self._open_search_menu()
+        self._click_dt_selection()
+        self._click_time_selection()
+
+        time_value = ""
+        click_counter = 0
+
+        # check if hour rolls over correctly from 23 to 0
+        while time_value != "0" and click_counter < 24:
+            self._next_hour()
+            time_value = self.wait.until(TextMatchesPattern((By.CSS_SELECTOR, '.rdtCount'), self.hour_pattern)).text
+            click_counter += 1
+        # assert the datetime value is equivalent to today's date at hour 0 and minute 0
+        assert self.dt_selection.get_attribute("value") == self.current_time.replace(hour=0, minute=0).strftime(
+            '%d/%m/%Y %H:%M')
+
+    def test_hour_rollover_downwards(self):
+        self._navigate_to_site()
+        self._open_search_menu()
+        self._click_dt_selection()
+        self._click_time_selection()
+
+        time_value = ""
+        click_counter = 0
+
+        # check if hour rolls over correctly from 0 to 23
+        while time_value != "23" and click_counter < 24:
+            self._prev_hour()
+            time_value = self.wait.until(TextMatchesPattern((By.CSS_SELECTOR, '.rdtCount'), self.hour_pattern)).text
+            click_counter += 1
+        # assert the datetime value is equivalent to today's date at hour 23 and minute 0
+        assert self.dt_selection.get_attribute("value") == self.current_time.replace(hour=23, minute=0).strftime(
+            '%d/%m/%Y %H:%M')
+
+    def test_last_day(self):
+        self._navigate_to_site()
+        self._open_search_menu()
+        self._click_dt_selection()
+
+        # navigate to the month with the last day that can be selected
+        if self.upper_bound_time.month > self.current_time.month or self.upper_bound_time.year > self.current_time.year:
+            self._next_month()
+            # click on the last day
+            self._click_on_date(self.upper_bound_time.day, self.upper_bound_time.month, self.upper_bound_time.year)
+
+
