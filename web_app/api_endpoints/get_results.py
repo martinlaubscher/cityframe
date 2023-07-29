49716590a17a42df1c978ed206047c709391a88c
Martin Laubscher <martin.laubscher@me.com>
2023-07-29 18:35:47 +0100

    implemented MAIRCA MCDM method with entropy weights to find top 10 results

--- web_app/api_endpoints/get_results.py
+++ web_app/api_endpoints/get_results.py
@@ -5,32 +5,27 @@ import django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'web_app.settings_dev'
 django.setup()
 
-from django.db.models import F
 from django.core.exceptions import FieldDoesNotExist, ObjectDoesNotExist
 from django.utils.timezone import is_aware
 from api_endpoints.models import TaxiZones, Busyness, WeatherFc, Results
 from dateutil import tz
 from datetime import datetime
 from itertools import islice
-import pytz
+from pymcdm.weights import entropy_weights
+from pymcdm.methods import MAIRCA
+from pymcdm.helpers import rankdata
+import numpy as np
 
 
 def check_error_type(e):
     """
     Checks the type of the raised exception and returns a corresponding message.
 
-    This function takes as input an exception object, checks if it is an instance of
-    Django's ObjectDoesNotExist or FieldDoesNotExist exceptions, and returns a string
-    message indicating the type of exception caught. If the exception is neither
-    ObjectDoesNotExist nor FieldDoesNotExist, it returns a message indicating that an
-    unknown exception has been caught.
-
     Args:
         e (Exception): The exception object to be checked.
 
     Returns:
         str: A string message indicating the type of the caught exception.
-
     """
 
     if isinstance(e, ObjectDoesNotExist):
@@ -41,95 +36,94 @@ def check_error_type(e):
         return f"Caught an unknown exception.\n{e}"
 
 
-def normalise(value, min_value, max_value):
-    """Normalises a value within a specified range.
-
-    This function takes a value and the minimum and maximum values of the range,
-    and returns the normalised value within the range [0, 1].
+def get_ny_dt(target_dt=datetime.now(tz=tz.gettz('America/New_York'))):
+    """
+    Converts the given target date and time to a New York timezone-aware datetime object.
 
     Args:
-        value (float): The value to be normalised.
-        min_value (float): The minimum value of the range.
-        max_value (float): The maximum value of the range.
+        target_dt (str or datetime, optional): The desired date and time in "YYYY-MM-DD HH:MM" format or a datetime object.
+            Defaults to the current time in the New York timezone.
 
     Returns:
-        float: The normalised value within the range [0, 1].
+        datetime: A datetime object representing the time in the New York timezone.
 
     Raises:
-        ZeroDivisionError: If `max_value` is equal to `min_value`.
+        ValueError: If the input is neither a correctly formatted string nor a datetime object.
     """
 
-    return (value - min_value) / (max_value - min_value)
+    # creating a datetime object from the string received by the post request.
+    # if the input is neither a correctly formatted string nor a datetime object, raise an error
+    if isinstance(target_dt, str):
+        ny_dt = datetime.strptime(target_dt, "%Y-%m-%d %H:%M").replace(minute=0)
+    elif isinstance(target_dt, datetime):
+        ny_dt = target_dt.replace(minute=0, second=0, microsecond=0)
+    else:
+        raise ValueError("Invalid date input. Expected a string or datetime object.")
+
+    # setting timezone to ny. if the supplied value/object is tz aware, convert it. if it is naive, assume it's ny time.
+    if is_aware(ny_dt):
+        return ny_dt.astimezone(tz.gettz('America/New_York'))
+    else:
+        return ny_dt.replace(tzinfo=tz.gettz('America/New_York'))
 
 
-def get_results(style, tree_range, busyness_range, user_time):
+def get_results(style, tree_range=(1, 5), busyness_range=(1, 5), user_time=get_ny_dt()):
     """
-    Fetches records associated with the architectural style and busyness range at a specific time.
+    Fetches records associated with the given architectural style, tree range, busyness range, and user time.
 
     Args:
-        style (str): Architectural style. It should match with the architectural style fields in the taxi_zones table.
-        busyness_range (tuple): A tuple of two integers indicating the lower and upper bounds of the busyness range.
-        tree_range (tuple): A tuple of two integers indicating the lower and upper bounds of the tree range.
-        user_time (str | datetime): The desired time in "YYYY-MM-DD HH:MM" format or as a datetime object.
+        style (str): Architectural style. It should match with the architectural style fields in the TaxiZones table.
+        tree_range (tuple, optional): A tuple of two integers indicating the lower and upper bounds of the tree range. Defaults to (1, 5).
+        busyness_range (tuple, optional): A tuple of two integers indicating the lower and upper bounds of the busyness range. Defaults to (1, 5).
+        user_time (datetime, optional): The desired time as a timezone-aware datetime object. Defaults to the current time in New York timezone.
 
     Returns:
-        dict: A dictionary containing information about each taxi zone including zone details, busyness level,
-        number of trees, architectural style count, and weather information.
+        dict: A dictionary containing information about each TaxiZone including zone details, busyness level, number of trees, architectural style count, and weather information.
     """
 
     # Get TaxiZones IDs having at least one building in the desired style and within the desired trees_scaled range
-    top_zones_ids = TaxiZones.objects.filter(**{style + '__gt': 0}, trees_scaled__range=tree_range).order_by(
-        F(style).desc()).values_list('id', flat=True)
-
+    top_zones_ids = TaxiZones.objects.filter(**{style + '__gt': 0}, trees_scaled__range=tree_range).values_list('id',
+                                                                                                                flat=True)
     # Get all Busyness records within the given busyness range and associated with the top zones
     records = Busyness.objects.filter(bucket__range=busyness_range, taxi_zone__in=top_zones_ids,
                                       dt_iso__dt_iso=user_time)
 
     results = {}
 
-    # Iterate over the records
     for record in records:
-        # Get the associated TaxiZone
         taxi_zone = TaxiZones.objects.get(id=record.taxi_zone.id)
-
         # If this zone id is not in results yet, add a new dictionary with zone's details
-        try:
-            if record.taxi_zone.id not in results:
-                results[record.taxi_zone.id] = {
-                    'zone': record.taxi_zone.zone,
-                    'dt_iso': record.dt_iso.dt_iso.astimezone(tz.gettz('America/New_York')).strftime(
-                        '%Y-%m-%d %H:%M'),
-                    'busyness': record.bucket,
-                    'trees': taxi_zone.trees_scaled,
-                    'style': getattr(taxi_zone, style),
-                    'weather': {
-                        'temp': record.dt_iso.temp,
-                        'weather_description': record.dt_iso.weather_description,
-                        'weather_icon': record.dt_iso.weather_icon
-                    }
-                }
-
-        except ObjectDoesNotExist as e:
-            return e
-
+        if record.taxi_zone.id not in results:
+            results[record.taxi_zone.id] = {
+                'zone': record.taxi_zone.zone,
+                'dt_iso': record.dt_iso.dt_iso.astimezone(tz.gettz('America/New_York')).strftime('%Y-%m-%d %H:%M'),
+                'busyness': record.bucket,
+                'trees': taxi_zone.trees_scaled,
+                'style': getattr(taxi_zone, style),
+                'weather': {'temp': record.dt_iso.temp, 'weather_description': record.dt_iso.weather_description,
+                            'weather_icon': record.dt_iso.weather_icon}
+            }
     return results
 
 
-def generate_response(target_busyness, target_trees, target_style, target_dt):
+def generate_response(target_busyness, target_trees, target_style, target_dt, mcdm_method=MAIRCA,
+                      mcdm_weights=entropy_weights):
     """
-    Generates a dictionary containing top 10 results based on the target busyness, style, and date-time.
+    Generates a dictionary containing top results based on the target busyness, style, and date-time.
 
     Args:
         target_busyness (int): The desired level of busyness.
         target_trees (int): The desired level of trees.
         target_style (str): The desired architectural style. It should match with one of the architectural styles in the taxi_zones table.
-        target_dt (str | datetime): The desired date and time in "YYYY-MM-DD HH:MM" format or a datetime object.
+        target_dt (str or datetime): The desired date and time in "YYYY-MM-DD HH:MM" format or a datetime object.
+        mcdm_method (callable, optional): The multi-criteria decision-making method to be used. Defaults to MAIRCA.
+        mcdm_weights (callable, optional): A function to calculate weights for the MCDM method. Defaults to entropy_weights.
 
     Returns:
-        dict: A dictionary containing the top 10 results sorted according to the difference from the target busyness
-        level and the count of the target style. The dictionary includes details of each zone such as the zone name,
-        level and thdeltae count of the target style. The dictionary includes details of each zone such as the zone name,
-        date and time, busyness level, number of trees, count of the target architectural style, and weather information.
+        dict: A dictionary containing the top 10 results.
+
+    Raises:
+        FieldDoesNotExist: If the target_style is not a valid architectural style.
     """
 
     # dictionary to translate the style names to django model variables
@@ -151,20 +145,7 @@ def generate_response(target_busyness, target_trees, target_style, target_dt):
         raise FieldDoesNotExist(
             f"The style '{target_style}' is invalid. It should be one of these: {tuple(style_dict.keys())}")
 
-    # creating a datetime object from the string received by the post request.
-    # if the input is neither a correctly formatted string nor a datetime object, raise an error
-    if isinstance(target_dt, str):
-        ny_dt = datetime.strptime(target_dt, "%Y-%m-%d %H:%M").replace(minute=0)
-    elif isinstance(target_dt, datetime):
-        ny_dt = target_dt.replace(minute=0, second=0, microsecond=0)
-    else:
-        raise ValueError("Invalid date input. Expected a string or datetime object.")
-
-    # setting timezone to ny. if the supplied value/object is tz aware, convert it. if it is naive, assume it's ny time.
-    if is_aware(ny_dt):
-        ny_dt = ny_dt.astimezone(tz.gettz('America/New_York'))
-    else:
-        ny_dt = ny_dt.replace(tzinfo=tz.gettz('America/New_York'))
+    ny_dt = get_ny_dt(target_dt)
 
     # if the time supplied is earlier than the earliest available time, take that one
     # if the time supplied is later than the latest available time, take that one
@@ -172,42 +153,50 @@ def generate_response(target_busyness, target_trees, target_style, target_dt):
     latest_dt_iso = WeatherFc.objects.latest('dt_iso').dt_iso
     ny_dt = max(min(ny_dt, latest_dt_iso), earliest_dt_iso)
 
-    results = get_results(style_dict.get(target_style), (1, 5), (1, 5), ny_dt)
+    results = get_results(style_dict.get(target_style), user_time=ny_dt)
 
     # check for errors
     if isinstance(results, Exception):
         return check_error_type(results)
 
-    max_building_count = max(value['style'] for value in results.values())
+    # prep alternatives with values for three criteria used in mcdm method in a numpy array:
+    # difference to target busyness level, difference to target tree level, and building count
+    alts = np.array([[abs(target_busyness - value['busyness']),
+                      abs(target_trees - value['trees']),
+                      value['style']] for value in results.values()], dtype=int)
 
-    # calculate the normalised scores for busyness, trees, buildings, and total
-    for key, value in results.items():
-        busyness_diff = abs(target_busyness - value['busyness'])
-        tree_diff = abs(target_trees - value['trees'])
-        busyness_score = 1 - normalise(busyness_diff, 0, 4)
-        tree_score = 1 - normalise(tree_diff, 0, 4)
-        building_score = normalise(value['style'], 0, max_building_count)
+    # define types of criteria (-1 for minimisation, 1 for maximisation)
+    types = np.array([-1, -1, 1])
 
-        value['score'] = (1 / 2) * busyness_score + (1 / 4) * tree_score + (1 / 4) * building_score
+    # set weights for criteria (default is entropy_weights)
+    weights = mcdm_weights(alts)
 
-    sorted_dict = dict(sorted(results.items(), key=lambda item: -item[1]['score']))
+    # initialise mcdm method (default is MAIRCA)
+    method = mcdm_method()
 
-    sliced_dict = dict(islice(sorted_dict.items(), 10))
+    # determine preferences and ranking for alternatives
+    pref = method(alts, weights, types)
+    ranking = rankdata(pref)
 
-    i = 1
-    # add the rank
-    for value in sliced_dict.values():
-        # print(f'Zone: {value["zone"]}\nScore: {value["score"]}\nTime: {value["dt_iso"]}')
-        value['rank'] = i
-        i += 1
+    # assign rankings to results
+    for idx, key in enumerate(results.keys()):
+        results[key]['rank'] = ranking[idx]
 
-    return sliced_dict
+    # sort results by rank
+    sorted_results = {k: v for k, v in sorted(results.items(), key=lambda item: item[1]['rank'])}
+
+    # keep only top 10
+    top_results = dict(islice(sorted_results.items(), 10))
+
+    # update the rank to be 1-10 for the top results (without repetitions / omissions)
+    for idx, key in enumerate(top_results.keys()):
+        top_results[key]['rank'] = idx + 1
+
+    return top_results
 
 
 def current_busyness():
-    ny_tz = pytz.timezone('America/New_York')
-    ny_dt = datetime.now(ny_tz)
-    ny_dt = ny_dt.replace(minute=0, second=0, microsecond=0)
+    ny_dt = get_ny_dt()
 
     # find records where dt_iso = ny_dt
     results = Results.objects.filter(dt_iso=ny_dt)
