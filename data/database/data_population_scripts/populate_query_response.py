bd43e01dc9b228627a05a5c7e72dd7a61e8c05f3
jmck90 <123461637+jmck90@users.noreply.github.com>
2023-07-30 22:38:12 +0100

    Update populate_query_response.py
    To run as a script (in Dev env this requires server to be running)

--- data/database/data_population_scripts/populate_query_response.py
+++ data/database/data_population_scripts/populate_query_response.py
@@ -1,46 +1,58 @@
+import os
+import sys
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_path)
+
 import random
 from datetime import datetime
 import pytz
 import requests
 
-# Define the possible styles
-styles = ["neo-Georgian", "Beaux-Arts", "Renaissance Revival", "neo-Grec",
-          "Romanesque Revival", "Greek Revival", "Queen Anne", "Italianate",
-          "neo-Renaissance", "Federal"]
 
-# Define the New York timezone
-ny_tz = pytz.timezone('America/New_York')
+def generate_queries():
+    # Define possible styles
+    styles = ["neo-Georgian", "Beaux-Arts", "Renaissance Revival", "neo-Grec",
+              "Romanesque Revival", "Greek Revival", "Queen Anne", "Italianate",
+              "neo-Renaissance", "Federal"]
 
-# Initialize the random number generator
-random.seed()
+    # Define the New York timezone
+    ny_tz = pytz.timezone('America/New_York')
 
-# Define the URL for the POST request
-url = "http://127.0.0.1:8000/api/submit-main"
+    # Initialize the random number generator
+    random.seed()
 
-# Generate 10 queries
-for _ in range(10):
-    # Generate random values for busyness, trees, and style
-    busyness = random.randint(1, 5)
-    trees = random.randint(1, 5)
-    style = random.choice(styles)
+    # Define the URL for the POST request
+    url = "http://127.0.0.1:8000/api/submit-main"
 
-    # Generate a random time for today in New York
-    now = datetime.now(ny_tz)
-    random_time = now.replace(hour=random.randint(0, 23), minute=0, second=0)
+    # Generate 10 queries
+    for _ in range(10):
+        # Generate random values for busyness, trees, and style
+        busyness = random.randint(1, 5)
+        trees = random.randint(1, 5)
+        style = random.choice(styles)
 
-    # Create the query
-    query = {
-        "busyness": busyness,
-        "trees": trees,
-        "time": random_time.strftime("%Y-%m-%d %H:%M"),
-        "style": style
-    }
+        # Generate a random time for today in New York
+        now = datetime.now(ny_tz)
+        random_time = now.replace(hour=random.randint(0, 23), minute=0, second=0)
 
-    # Send the POST request
-    response = requests.post(url, data=query)
+        # Create the query
+        query = {
+            "busyness": busyness,
+            "trees": trees,
+            "time": random_time.strftime("%Y-%m-%d %H:%M"),
+            "style": style
+        }
 
-    # Check the status of the request
-    if response.status_code == 200:
-        print(f"Successfully sent query: {query}")
-    else:
-        print(f"Failed to send query: {query}. Status code: {response.status_code}")
+        # Send the POST request
+        response = requests.post(url, data=query)
+
+        # Check the status of the request
+        if response.status_code == 200:
+            print(f"Successfully sent query: {query}")
+        else:
+            print(f"Failed to send query: {query}. Status code: {response.status_code}")
+
+
+if __name__ == '__main__':
+    generate_queries()
