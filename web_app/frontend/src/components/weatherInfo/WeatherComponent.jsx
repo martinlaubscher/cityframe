9ec70faa6d6f320ea788d9624ea3e397c27b0c92
Jingao <jingao.liu13@gmail.com>
2023-07-31 09:07:21 +0100

    weather desc

--- web_app/frontend/src/components/weatherInfo/WeatherComponent.jsx
+++ web_app/frontend/src/components/weatherInfo/WeatherComponent.jsx
@@ -16,17 +16,25 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./WeatherInfoCSS.css";
 function WeatherComponent() {
-  const [weather, setWeather] = useState(null);
 
-
-// Frontend: Implemented CORS in the local environment to facilitate real-time visualization
-// of changes made to the frontend code. This allows the frontend to instantly view the modifications
-// on the web page while utilizing data obtained from the backend, eliminating the need to run "npm build"
-// and handle static files every time.
-useEffect(() => {
+    const [weather, setWeather] = useState(null);
+    const [weatherDescription, setWeatherDescription] = useState('');
+  
+    const handleClick = (description) => {
+      if(weatherDescription === description) {
+        setWeatherDescription('');
+      } else {
+        setWeatherDescription(description);
+      }
+    };
+  // Frontend: Implemented CORS in the local environment to facilitate real-time visualization
+  // of changes made to the frontend code. This allows the frontend to instantly view the modifications
+  // on the web page while utilizing data obtained from the backend, eliminating the need to run "npm build"
+  // and handle static files every time.
+  useEffect(() => {
     axios
-      .get("/api/current-weather/") //Notice：de-comment in final version
-      // .get("http://127.0.0.1:8000/api/current-weather/") //Notice：comment in final version
+      // .get("/api/current-weather/") //Notice：de-comment in final version
+      .get("http://127.0.0.1:8000/api/current-weather/") //Notice：comment in final version
       .then((response) => {
         setWeather(response.data);
       })
@@ -36,61 +44,62 @@ useEffect(() => {
   }, []);
 
   return weather ? (
-    <div>
+    <div className="weather-icon-container">
       {weather.weather[0].icon === "01d" && (
-        <FontAwesomeIcon icon={faSun} className="weather-icon" />
+        <FontAwesomeIcon icon={faSun} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "01n" && (
-        <FontAwesomeIcon icon={faMoon} className="weather-icon" />
+        <FontAwesomeIcon icon={faMoon} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "02d" && (
-        <FontAwesomeIcon icon={faCloudSun} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloudSun} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "02n" && (
-        <FontAwesomeIcon icon={faCloudMoon} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloudMoon} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "03d" && (
-        <FontAwesomeIcon icon={faCloud} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloud} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "03n" && (
-        <FontAwesomeIcon icon={faCloud} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloud} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "04d" && (
-        <FontAwesomeIcon icon={faCloud} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloud} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "04n" && (
-        <FontAwesomeIcon icon={faCloud} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloud} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "09d" && (
-        <FontAwesomeIcon icon={faCloudShowersHeavy} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloudShowersHeavy} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "09n" && (
-        <FontAwesomeIcon icon={faCloudShowersHeavy} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloudShowersHeavy} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "10d" && (
-        <FontAwesomeIcon icon={faCloudSunRain} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloudSunRain} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "10n" && (
-        <FontAwesomeIcon icon={faCloudMoonRain} className="weather-icon" />
+        <FontAwesomeIcon icon={faCloudMoonRain} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "11d" && (
-        <FontAwesomeIcon icon={faBolt} className="weather-icon" />
+        <FontAwesomeIcon icon={faBolt} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "11n" && (
-        <FontAwesomeIcon icon={faBolt} className="weather-icon" />
+        <FontAwesomeIcon icon={faBolt} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "13d" && (
-        <FontAwesomeIcon icon={faSnowflake} className="weather-icon" />
+        <FontAwesomeIcon icon={faSnowflake} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "13n" && (
-        <FontAwesomeIcon icon={faSnowflake} className="weather-icon" />
+        <FontAwesomeIcon icon={faSnowflake} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "50d" && (
-        <FontAwesomeIcon icon={faSmog} className="weather-icon" />
+        <FontAwesomeIcon icon={faSmog} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
       {weather.weather[0].icon === "50n" && (
-        <FontAwesomeIcon icon={faSmog} className="weather-icon" />
+        <FontAwesomeIcon icon={faSmog} className="weather-icon" onClick={() => handleClick(weather.weather[0].description)} />
       )}
+      {weatherDescription && <div className="weather-description-popup">{weatherDescription}</div>}
     </div>
   ) : (
     <div>Loading...</div>
