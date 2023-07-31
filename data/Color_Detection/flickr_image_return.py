2f997cc6cdd0d92c2d05cfc06b37b3914f3fa903
jsmo998 <123461587+jsmo998@users.noreply.github.com>
2023-07-31 13:29:53 +0100

    unique images returned from flickr for each zone

--- data/Color_Detection/flickr_image_return.py
+++ data/Color_Detection/flickr_image_return.py
@@ -1,4 +1,15 @@
+import os
+import sys
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+cityframe_path = os.path.dirname(os.path.dirname(current_path))
+taxi_path = os.path.join(current_path, "..", "GeoJSON", "taxi_zones_with_centroid.geojson")
+
+sys.path.append(cityframe_path)
+
 import requests
+import geopandas as gpd
+from credentials import flickr_api_key
 
 def get_flickr_image(api_key, lat, lon):
     base_url = "https://api.flickr.com/services/rest/"
@@ -10,10 +21,12 @@ def get_flickr_image(api_key, lat, lon):
         "nojsoncallback": 1,
         "lat": lat,
         "lon": lon,
-        "radius": 5,  # Radius in km. You can adjust this value as needed.
+        "radius": 2,  # Radius in km. You can adjust this value as needed.
         "sort": "relevance",
         "per_page": 1,  # You can adjust the number of images returned here.
-        "geo_context": 2  # outdoor photos
+        "geo_context": 2,  # outdoor photos
+        "accuracy": 14,  # accuracy of location information (street level ~16)
+        "content_types": 0  # photos only
     }
 
     response = requests.get(base_url, params=params)
@@ -32,12 +45,21 @@ def get_flickr_image(api_key, lat, lon):
 
     return None
 
-# Replace 'YOUR_API_KEY' with your actual Flickr API key
-api_key = "5fdce5ee86d5986e46fb9477e27cc578"
-latitude = 40.758332724348996
-longitude = -73.98642379545548
+gdf = gpd.read_file(taxi_path)
+api_key = flickr_api_key
+print(gdf.shape)
+image_urls = []
 
-image_url = get_flickr_image(api_key, latitude, longitude)
+for index, row in gdf.iterrows():
+    point = row['point']
+    split = point.split()
+    lng = split[0]
+    lat = split[1]
+    image_url = get_flickr_image(api_key, lat, lng)
+    if image_url is None:
+        image_url = "https://i1.sndcdn.com/artworks-CyTzk0PMsjHFfr7D-S8wWcw-t500x500.jpg"
+    image_urls.append(image_url)
+print(len(image_urls))
+print(image_urls)
 
-if image_url:
-    print("Image URL:", image_url)
+
