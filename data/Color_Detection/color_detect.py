f844e86c8ed55d8d5c5641918e3d0a11a1e41bde
Martin Laubscher <martin.laubscher@me.com>
2023-07-31 16:23:34 +0100

    adjustments

--- data/Color_Detection/color_detect.py
+++ data/Color_Detection/color_detect.py
@@ -10,7 +10,6 @@ cityframe_path = os.path.dirname(os.path.dirname(current_path))
 zone_path = os.path.join(current_path, "..", "GeoJSON", "zones_with_images.geojson")
 sys.path.append(cityframe_path)
 
-
 import numpy as np
 import pandas as pd
 import extcolors
@@ -24,10 +23,8 @@ from io import BytesIO
 from credentials import pg_conn
 
 
-
-#loads in the image and extracts a basic RGB color palette
+# loads in the image and extracts a basic RGB color palette
 def img_load(url):
-
     """
     Args: filepath (String, valid file path)
     Returns: Extcolors object
@@ -37,33 +34,31 @@ def img_load(url):
     image = Image.open(BytesIO(response.content))
 
     color_palette = extcolors.extract_from_image(image, tolerance=12, limit=11)
-    return color_palette 
+    return color_palette
 
 
-
-#takes the RGB color palette and converts it to a dataframe of hex values, along with the frequency
+# takes the RGB color palette and converts it to a dataframe of hex values, along with the frequency
 def palette_check(input):
-
     """
     Args: input (Extcolors object)
     Returns: Hex code occurence df (Pandas dataframe)
     """
 
-    colors_pre_list = str(input).replace('([(','').split(', (')[0:-1]
+    colors_pre_list = str(input).replace('([(', '').split(', (')[0:-1]
     df_rgb = [i.split('), ')[0] + ')' for i in colors_pre_list]
-    df_percent = [i.split('), ')[1].replace(')','') for i in colors_pre_list]
-    
-    #convert RGB to HEX code
-    df_color_up = [rgb2hex(int(i.split(", ")[0].replace("(","")), int(i.split(", ")[1]), int(i.split(", ")[2].replace(")",""))) for i in df_rgb]
-    
-    df = pd.DataFrame(zip(df_color_up, df_percent), columns = ['c_code','occurence'])
+    df_percent = [i.split('), ')[1].replace(')', '') for i in colors_pre_list]
+
+    # convert RGB to HEX code
+    df_color_up = [
+        rgb2hex(int(i.split(", ")[0].replace("(", "")), int(i.split(", ")[1]), int(i.split(", ")[2].replace(")", "")))
+        for i in df_rgb]
+
+    df = pd.DataFrame(zip(df_color_up, df_percent), columns=['c_code', 'occurence'])
     return df['c_code'].to_list()
 
 
-
-#opens the geojson file with the taxi zone images, and populates the df with color frequencies
+# opens the geojson file with the taxi zone images, and populates the df with color frequencies
 def open_geo(geojson=zone_path):
-    
     """
     Args: Geojson File (containing taxi zones/ flickr image links)
     Returns: Pandas dataframe
@@ -71,18 +66,16 @@ def open_geo(geojson=zone_path):
 
     with open(geojson, 'r') as f:
         data = json.load(f)
-    
+
     zones = []
     image_urls = []
     colors = []
 
-
     features = data['features']
 
     for feature in features:
-
         properties = feature['properties']
-        zones.append(properties['zone'])
+        zones.append(properties['location_id'])
         image_urls.append(properties['image_url'])
 
         image = img_load(properties['image_url'])
@@ -92,21 +85,16 @@ def open_geo(geojson=zone_path):
     return df
 
 
-
 # creates a new database to hold results
-def create_db(self, df):
-
+def create_db(df):
     """
     Args: pandas dataframe
     Returns: None
     """
 
-    engine = create_engine(self.pg_url)
+    engine = create_engine(URL.create("postgresql+psycopg", **pg_conn))
 
     df.to_sql('Color_Results', engine, schema='cityframe', if_exists='replace', index=False)
 
-    return None
 
-
-
-df = create_db(open_geo())
+create_db(open_geo())
