b847f425e96ed258a882d10d5f8ab7e5b4b0273f
Jingao <jingao.liu13@gmail.com>
2023-08-01 20:24:06 +0100

    integrate homepage heatmap and result heatmap

--- web_app/frontend/src/components/mapBackground/Map.jsx
+++ web_app/frontend/src/components/mapBackground/Map.jsx
@@ -1,66 +1,61 @@
-import {MapContainer, TileLayer, ZoomControl, Marker, Popup, Polygon} from 'react-leaflet'
-import "./MapBackground.css"
-import React from 'react'
-import axios from '@/axiosConfig';
-
-//import geojsonData from '../dummydata/geodata'
+import {
+  MapContainer,
+  TileLayer,
+  Polygon,
+} from "react-leaflet";
+import "./MapBackground.css";
+import React from "react";
+import axios from "@/axiosConfig";
 
 export default function Map(props) {
+  const defaultOptions = {
+    color: "#808080",
+    weight: 1,
+    fillColor: "blue",
+    fillOpacity: 0,
+  };
 
-    const defaultOptions = {color: '#808080', weight: 1, fillColor:"blue", fillOpacity: 0}
-    //const purpleOptions = {color: "purple", weight: 2, fillColor:"green", fillOpacity: 0.5}
-    
-    const [geojsonData, setGeojsonData] = React.useState(null);
+  const [geojsonData, setGeojsonData] = React.useState(null);
 
-    React.useEffect(() => {
-        axios.get('/assets/manhattan_taxi_zones.geojson')
-            .then(response => {
-                setGeojsonData(response.data);
-            })
-            .catch(error => {
-                console.error("Error fetching geojson data: ", error);
-            });
-    }, []);
-    
+  React.useEffect(() => {
+    axios
+      .get("/assets/manhattan_taxi_zones.geojson")
+      .then((response) => {
+        setGeojsonData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching geojson data: ", error);
+      });
+  }, []);
 
+  function handleClick() {
+    console.log("She doesn't even go here");
+  }
 
-    function handleClick(){
-        console.log("She doesn't even go here")
+  function rankColour(rank) {
+    // Define the hue and lightness range for the heatmap. Saturation remains at 100%
+    const colourVar = 10 - rank;
+    //const hue = colourVar + 230; // PURPLE!!!
+    const hue = 110 + rank * 10;
+    console.log(rank, hue);
+
+    const lightness = 90 - colourVar * 5;
+    return `hsl(${hue}, ${100}%, ${50}%)`;
+  }
+
+  function rankOutline(rank) {
+    if (rank === 1) {
+      return "#E6BE00";
+    } else if (rank === 2) {
+      return "#D7D7D7";
+    } else if (rank === 3) {
+      return "#A55028";
+    } else {
+      return "#9B9169";
     }
+  }
 
-    function rankColour(rank) {
-
-        // Define the hue and lightness range for the heatmap. Saturation remains at 100%
-        const colourVar = (10 - rank)
-        //const hue = colourVar + 230; // PURPLE!!!
-        const hue = 110+rank*10
-        console.log(rank, hue)
-
-        const lightness = 90 - (colourVar * 5);
-        return `hsl(${hue}, ${100}%, ${50}%)`
-
-    }
-
-    function rankOutline(rank){
-        if (rank === 1){
-            return "#E6BE00"
-        }
-        else if (rank === 2){
-            return "#D7D7D7"
-        }
-        else if (rank === 3){
-            return "#A55028"
-        }
-        else{
-            return "#9B9169"
-        }
-    }
-    
-    function busyColour(busy){
-        const hue = busy*55
-        return `hsl(${hue}, ${100}%, ${50}%)`
-
-         // ======================homepage heatmap============================
+  // ======================homepage heatmap============================
   console.log("busynessZones-Map recieve the prop:", props.busynessZones);
 
   const getBusynessColor = (busynessLevel) => {
@@ -90,36 +85,44 @@ export default function Map(props) {
 
   // ================================================================================
 
-  
-  if (geojsonData){
-    var path
-    var click
-    if (props.isSearched){
+  var polygons;
+  if (geojsonData) {
+    var path;
+    var click;
+    if (props.isSearched) {
+      polygons = geojsonData.features.map((feature, idx) => {
+        var placeRank = props.searchResults.find(
+          (place) => place.id === feature.properties.location_id
+        );
 
-    polygons=geojsonData.features.map((feature, idx) => {
+        if (placeRank) {
+          path = {
+            color: "purple",
+            weight: 2,
+            fillColor: rankColour(placeRank.rank),
+            fillOpacity: 1,
+          };
+          click = () => props.buildlist(feature, placeRank);
+        } else {
+          path = defaultOptions;
+          click = handleClick;
+        }
 
-    
-    var placeRank = props.searchResults.find(place => place.id === feature.properties.location_id)
-
-    if (placeRank){
-        path= {color: "purple", weight: 2, fillColor:rankColour(placeRank.rank), fillOpacity: 1}
-        click= () => props.buildlist(feature, placeRank)
+        return feature.geometry.coordinates.map((polygon, polygonIndex) => {
+          return (
+            <Polygon
+              key={`${idx}-${polygonIndex}`}
+              positions={polygon[0].map((coord) => [coord[1], coord[0]])} // swap lat and lng
+              pathOptions={path}
+              eventHandlers={{
+                click: click,
+              }}
+            />
+          );
+        });
+      });
     }
-    else{
-        path=defaultOptions
-        click= handleClick
-    }
-
-    return feature.geometry.coordinates.map((polygon, polygonIndex) => {
-        return(
-        <Polygon
-            key={`${idx}-${polygonIndex}`}
-            positions={polygon[0].map(coord => [coord[1], coord[0]])} // swap lat and lng
-            pathOptions={path}
-            eventHandlers={{
-                click: click
-            }}
-            // ======================homepage heatmap============================
+    // ======================homepage heatmap============================
     else {
       polygons = geojsonData.features.map((feature, idx) => {
         var path;
@@ -134,14 +137,14 @@ export default function Map(props) {
             color: "black",
             weight: 1,
             fillOpacity: 0.9,
-            dashArray: '5, 5'
+            dashArray: "5, 5",
           };
         } else {
-          path = { 
-            color: "black", 
-            weight: 1, 
+          path = {
+            color: "black",
+            weight: 1,
             fillOpacity: 0,
-            dashArray: '5, 5' // add dash line style
+            dashArray: "5, 5", // add dash line style
           };
         }
 
@@ -167,7 +170,7 @@ export default function Map(props) {
         zoom={12}
         scrollWheelZoom={true}
         maxZoom={14}
-        minZoom={11}  
+        minZoom={11}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -177,5 +180,4 @@ export default function Map(props) {
       </MapContainer>
     </div>
   );
-    
 }
