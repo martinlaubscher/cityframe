af7d7197b7faefca898ecb04fa32d3e39099aed0
Jingao <jingao.liu13@gmail.com>
2023-08-05 17:23:53 +0100

    add about section

--- web_app/frontend/src/pages/AboutThisWeb.jsx
+++ web_app/frontend/src/pages/AboutThisWeb.jsx
@@ -1,17 +1,42 @@
-import"./navItemsCSS.css"
 export default function AboutThisWeb() {
-    return (
-        <>
+  return (
+    <div className="about-this-web-container">
+        <div className="section">
+            <span className="title">About</span>
             <p>
-                About Cityframe helps you find the perfect frame for your shot. Whether you're a professional filmmaker or an amateur photographer, use cityframe to discover Manhattan in ways you never have before. How to use:
+              Cityframe helps you find the perfect frame for your shot. Whether
+              you're a professional filmmaker or an amateur photographer, use
+              cityframe to discover Manhattan in ways you never have before.
             </p>
-            <ul>
-                <li>Explore Busyness</li>
-                <li>Use the slider on the homepage to see current busyness levels in Manhattan city zones.</li>
-                <li>Select search input parameters and get results of the top ten zones in the city which best match your input returned.</li>
-                <li>Read your search results data carefully. Zones which are returned may not match all your given search values. The accuracy of search results will vary depending on the input given. Unlikely searches, for example, high busyness in the middle of the night, will include results at more suitable times which match your other search parameters.</li>
-            </ul>
-            <p>Search in city zones</p>
-        </>
-    );
+        </div>
+
+        <div className="section">
+            <span className="title">How to use</span>
+
+            <div className="subsection">
+                <p className="subtitle">Explore Busyness</p>
+                <p>
+                  Use the slider on the homepage to see current busyness levels in
+                  Manhattan city zones.
+                </p>
+            </div>
+
+            <div className="subsection">
+                <p className="subtitle">Search in city zones</p>
+                <p>
+                    Select search input parameters and get results of the top ten zones
+                    in the city which best match your input returned.
+                </p>
+                <p>
+                    Read your search results data carefully. Zones which are returned
+                    may not match all your given search values. The accuracy of search
+                    results will vary depending on the input given. Unlikely searches,
+                    for example, high busyness in the middle of the night, will include
+                    results at more suitable times which match your other search
+                    parameters.
+                </p>
+            </div>
+        </div>
+    </div>
+  );
 }
