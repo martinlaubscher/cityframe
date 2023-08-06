4615d8443d590c9e8ad5c94d30702d503402977c
Martin Laubscher <martin.laubscher@me.com>
2023-08-06 13:20:13 +0100

    updated selenium tests - updated search test to work with new structure - added test for search with no results - added test for clear search button

--- data/Mapping_Buildings_and_Zones/prep_zoning_data.py
+++ data/Mapping_Buildings_and_Zones/prep_zoning_data.py
@@ -26,66 +26,69 @@ def remap_zonedist(value):
     else:
         return 'residential'
 
+def main():
+    # EPSG for NYC
+    nyc_crs = 'EPSG:2263'
 
-# EPSG for NYC
-nyc_crs = 'EPSG:2263'
+    # if the updated zoning file exists, use it. otherwise, create it first.
+    if os.path.isfile(updated_zoning_path):
+        zoning_gdf = gpd.read_file(updated_zoning_path).to_crs(nyc_crs)
+    else:
+        zoning_gdf = gpd.read_file(zoning_path).to_crs(nyc_crs)
+        zoning_gdf['zonedist'] = zoning_gdf['zonedist'].apply(remap_zonedist)
+        zoning_gdf.to_file(updated_zoning_path, driver='GeoJSON')
 
-# if the updated zoning file exists, use it. otherwise, create it first.
-if os.path.isfile(updated_zoning_path):
-    zoning_gdf = gpd.read_file(updated_zoning_path).to_crs(nyc_crs)
-else:
-    zoning_gdf = gpd.read_file(zoning_path).to_crs(nyc_crs)
-    zoning_gdf['zonedist'] = zoning_gdf['zonedist'].apply(remap_zonedist)
-    zoning_gdf.to_file(updated_zoning_path, driver='GeoJSON')
+    # Load the taxi zone GeoJSON and set the crs to the nyc one
+    taxi_gdf = gpd.read_file(taxi_path)
+    taxi_gdf = taxi_gdf.to_crs(nyc_crs)
 
-# Load the taxi zone GeoJSON and set the crs to the nyc one
-taxi_gdf = gpd.read_file(taxi_path)
-taxi_gdf = taxi_gdf.to_crs(nyc_crs)
+    # combine the three Governor's Island/Ellis Island/Liberty Island rows into one and delete the three old rows
+    three_islands = taxi_gdf.loc[taxi_gdf['location_id'] == '103', 'geometry'].unary_union
+    taxi_gdf = taxi_gdf[taxi_gdf['location_id'] != '103']
+    three_islands_row = gpd.GeoDataFrame(pd.DataFrame([{'location_id': '103',
+                                                        'geometry': three_islands,
+                                                        'zone': 'Governor\'s Island/Ellis Island/Liberty Island'}]),
+                                         geometry='geometry')
 
-# combine the three Governor's Island/Ellis Island/Liberty Island rows into one and delete the three old rows
-three_islands = taxi_gdf.loc[taxi_gdf['location_id'] == '103', 'geometry'].unary_union
-taxi_gdf = taxi_gdf[taxi_gdf['location_id'] != '103']
-three_islands_row = gpd.GeoDataFrame(pd.DataFrame([{'location_id': '103',
-                                                    'geometry': three_islands,
-                                                    'zone': 'Governor\'s Island/Ellis Island/Liberty Island'}]),
-                                     geometry='geometry')
+    # set the crs for the new (combined) row
+    three_islands_row.set_crs(nyc_crs, inplace=True)
 
-# set the crs for the new (combined) row
-three_islands_row.set_crs(nyc_crs, inplace=True)
+    # append the new row to the existing df
+    taxi_gdf = gpd.GeoDataFrame(pd.concat([taxi_gdf, three_islands_row])).reset_index(drop=True)
 
-# append the new row to the existing df
-taxi_gdf = gpd.GeoDataFrame(pd.concat([taxi_gdf, three_islands_row])).reset_index(drop=True)
+    # spatial join between taxi zones and zoning districts
+    joined_gdf = gpd.sjoin(taxi_gdf, zoning_gdf, how='left', predicate='intersects')
 
-# spatial join between taxi zones and zoning districts
-joined_gdf = gpd.sjoin(taxi_gdf, zoning_gdf, how='left', predicate='intersects')
+    # calculate intersection between taxi zones and zoning districts
+    joined_gdf['intersection'] = joined_gdf.apply(
+        lambda row: row['geometry'].intersection(zoning_gdf.loc[row['index_right'], 'geometry']) if pd.notnull(
+            row['index_right']) else None, axis=1)
 
-# calculate intersection between taxi zones and zoning districts
-joined_gdf['intersection'] = joined_gdf.apply(
-    lambda row: row['geometry'].intersection(zoning_gdf.loc[row['index_right'], 'geometry']) if pd.notnull(
-        row['index_right']) else None, axis=1)
+    # convert intersection to GeoSeries (so that the area can be calculated)
+    joined_gdf['intersection'] = gpd.GeoSeries(joined_gdf['intersection'], crs=nyc_crs)
 
-# convert intersection to GeoSeries (so that the area can be calculated)
-joined_gdf['intersection'] = gpd.GeoSeries(joined_gdf['intersection'], crs=nyc_crs)
+    # calculate the intersection area
+    joined_gdf['area'] = joined_gdf['intersection'].area
 
-# calculate the intersection area
-joined_gdf['area'] = joined_gdf['intersection'].area
+    # group the df, summing the area per zone/district type
+    result_df = joined_gdf.groupby(['location_id', 'zonedist']).agg({'area': 'sum'}).reset_index()
+    # calculate how much of each zone is what district type
+    result_df['percent'] = result_df.groupby('location_id')['area'].transform(lambda x: x / x.sum() * 100)
+    result_df = result_df.drop(columns=['area'])
 
-# group the df, summing the area per zone/district type
-result_df = joined_gdf.groupby(['location_id', 'zonedist']).agg({'area': 'sum'}).reset_index()
-# calculate how much of each zone is what district type
-result_df['percent'] = result_df.groupby('location_id')['area'].transform(lambda x: x / x.sum() * 100)
-result_df = result_df.drop(columns=['area'])
+    result_df['location_id'] = result_df['location_id'].astype(int)
 
-result_df['location_id'] = result_df['location_id'].astype(int)
+    # pivot the results so that we have a df with the location id as index
+    # and the percentage for each district type as the columns
+    final_df = pd.pivot_table(result_df, values='percent', index=['location_id'], columns=['zonedist'], aggfunc='sum',
+                              fill_value=0).sort_index()
 
-# pivot the results so that we have a df with the location id as index
-# and the percentage for each district type as the columns
-final_df = pd.pivot_table(result_df, values='percent', index=['location_id'], columns=['zonedist'], aggfunc='sum',
-                          fill_value=0).sort_index()
+    # add the name of the zone type with the highest value
+    final_df['zone_type'] = final_df.iloc[:].idxmax(axis=1)
 
-# add the name of the zone type with the highest value
-final_df['zone_type'] = final_df.iloc[:].idxmax(axis=1)
+    engine = create_engine(URL.create("postgresql+psycopg", **pg_conn))
 
-engine = create_engine(URL.create("postgresql+psycopg", **pg_conn))
+    final_df.to_sql('zoning', engine, schema='cityframe', if_exists='replace')
 
-final_df.to_sql('zoning', engine, schema='cityframe', if_exists='replace')
+if __name__ == main():
+    main()
