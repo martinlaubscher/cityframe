07bb291af37b863527a4a98ade1903f831441cb7
Jingao <jingao.liu13@gmail.com>
2023-08-07 14:04:07 +0100

    optimize contact

--- web_app/frontend/src/pages/Contact.jsx
+++ web_app/frontend/src/pages/Contact.jsx
@@ -9,12 +9,17 @@ export default function Contact() {
         <div className="general-support">
           <h2>General Support</h2>
           <p>Mojo Dojo Casa House</p>
-          <p>Email: team@cityfra.me</p>
+          <p>or email us at team@cityfra.me</p>
         </div>
         <div className="support">
           <h2>Support</h2>
-          <p>For questions or help using Cityframe:</p>
-          <p>Email: help@cityfra.me</p>
+          <p>For questions or help using Cityframe,</p>
+          <p>contact help@cityfra.me</p>
+        </div>
+        <div className="press">
+          <h2>Press</h2>
+          <p>Send any press inquiries to</p>
+          <p>press@cityfra.me</p>
         </div>
       </div>
     </div>
