486980d212d4adc4754162a0d3d451cd1be7b215
jsmo998 <123461587+jsmo998@users.noreply.github.com>
2023-08-10 13:01:04 +0100

    updated links in contact

--- web_app/frontend/src/pages/Contact.jsx
+++ web_app/frontend/src/pages/Contact.jsx
@@ -7,21 +7,21 @@ export default function Contact() {
       </p>
       <div className="contact-methods">
         <div className="general-support">
-          <h2>general</h2>
-          <p>mojo dojo casa house</p>
-          <p>or email us at team@cityfra.me</p>
+          <h2>general.</h2>
+          <a className="mojo" href="javascript:void(0)">mojo dojo casa house </a>
+            <p>for general feedback and comments, please email us at <a href="mailto:team@cityfra.me">team@cityfra.me</a></p>
         </div>
         <div className="support">
-          <h2>support</h2>
-          <p>For questions or help using CITYFRAME,</p>
-          <p>contact help@cityfra.me</p>
+          <h2>support.</h2>
+            <p>for questions or help using CITYFRAME contact our support helpline <a href="mailto:help@cityfra.me">here</a></p>
         </div>
         <div className="press">
-          <h2>business</h2>
-          <p>send any business inquiries to</p>
-          <p>press@cityfra.me</p>
+          <h2>business.</h2>
+            <p>please send any business inquiries to <a href="mailto:press@cityfra.me">our business email</a></p>
         </div>
       </div>
+      <br></br>
+      <h3>thank you for visiting our app.</h3>
     </div>
   );
 }
